b9dadbed738b4741ba269b0773f1a823ae79b20a
Matthew Ryckembusch <matryck@outlook.fr>
2026-09-06 23:55:29 +0200

    Scene Manager & Delta Time Variable

--- Mario/src/main/java/jade/Window.java
+++ Mario/src/main/java/jade/Window.java
@@ -3,6 +3,7 @@ package jade;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
+import util.Time;
 
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
@@ -15,9 +16,10 @@ public class Window
     private long glfwWindow;
     private boolean fadeToBlack = false;
 
-    private float r, g, b, a;
+    public float r, g, b, a;
 
     private static Window window = null;
+    private static Scene currentScene;
 
     private Window()
     {
@@ -28,6 +30,25 @@ public class Window
         g = 1;
         b = 1;
         a = 1;
+    }
+
+    public static void changeScene(int newScene)
+    {
+        switch(newScene)
+        {
+            case 0:
+                currentScene = new LevelEditorScene();
+                // currentScene.init();
+                break;
+
+            case 1:
+                currentScene = new LevelScene();
+                break;
+
+            default:
+                assert false: "Unknown scene '" + newScene + "'";
+                break;
+        }
     }
 
     public static Window get()
@@ -90,11 +111,24 @@ public class Window
         // Make the window visible
         glfwShowWindow(glfwWindow);
 
+        /*
+        * This line is critical for LWJGL's interoperation with GLFW's
+        * OpenGL context, or any context that is managed externally
+        * LWJGL detects the context that is current in the current thread,
+        * creates the GLCapabilities instance and makes the OpenGL
+        * bindings available for use
+        * */
         GL.createCapabilities();
+
+        Window.changeScene(0);
     }
 
     public void loop()
     {
+        float beginTime = Time.getTime();
+        float endTime;
+        float dt = -1.0f;
+
         while(!glfwWindowShouldClose(glfwWindow))
         {
             // Poll Events
@@ -103,19 +137,16 @@ public class Window
             glClearColor(r, g, b, a);
             glClear(GL_COLOR_BUFFER_BIT);
 
-            if(fadeToBlack)
+            if(dt >= 0)
             {
-                r = Math.max(r - 0.01f, 0);
-                g = Math.max(g - 0.01f, 0);
-                b = Math.max(b - 0.01f, 0);
-            }
-
-            if(KeyListener.isKeyPressed(GLFW_KEY_SPACE))
-            {
-                fadeToBlack = true;
+                currentScene.update(dt);
             }
 
             glfwSwapBuffers(glfwWindow);
+
+            endTime = Time.getTime();
+            dt = endTime - beginTime;
+            beginTime = endTime;
         }
     }
 
